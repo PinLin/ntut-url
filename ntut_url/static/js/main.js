f96cb340aa162469067b0a0e2c4fc553f78a0e83
Pin Lin <moneycat711@gmail.com>
2019-02-15 01:37:42 +0800

    Browse url

---
+++ ntut_url/static/js/main.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let domain = (new URL(location.href)).origin
+
+const loadPage = async () => {
+    let data = localStorage.getItem('profile')
+    if (data) {
+        let profile = JSON.parse(data)
+
+        $('#loginBox .secret input')[0].value = profile.secret
+
+        await loginSystem()
+    }
+}
+
+const loginSystem = async () => {
+    let secret = $('#loginBox .secret input')[0].value
+
+    if (!secret) {
+        alert("Secret field is empty.")
+        return
+    }
+
+    try {
+        let res = await callBrowse(secret)
+        console.log(res)
+
+        localStorage.setItem('profile', JSON.stringify({ secret }))
+    }
+    catch (err) {
+        console.log(err)
+
+        if (err.status == 401) {
+            alert("Verification failed.")
+            return
+        }
+
+        alert("Unable to establish connection.")
+    }
+}
+
+const callBrowse = (secret) => {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: 'GET',
+            url: `${domain}/api/browse`,
+            headers: { Secret: secret },
+            success: (res) => { resolve(res) },
+            error: (err) => { reject(err) }
+        })
+    })
+}
