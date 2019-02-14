3a4b651e03d8e82a09828ae26235819355efef87
Pin Lin <moneycat711@gmail.com>
2019-02-14 23:18:20 +0800

    Change port by environment variable

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+import os
 from ntut_url import app
 
 
 def main():
-    app.run(threaded=True)
+    SERVER_PORT = int(os.environ.get('SERVER_PORT') or 5000)
+
+    app.run(port=SERVER_PORT, threaded=True)
 
 
 if __name__ == '__main__':
