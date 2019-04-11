4dd923fb994b608ce081c67a77416a7f8b1f4f24
Pin Lin <moneycat711@gmail.com>
2019-04-11 17:36:49 +0800

    Rename routes.setting to routes.api

--- ntut_url/__init__.py
+++ ntut_url/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from .models.url import Url
-from .routes.setting import app as setting_route
+from .routes.api import app as api_route
 from .routes.redirect import app as redirect_route
 
 Url.create_table()
 
 app = Flask(__name__)
-app.register_blueprint(setting_route, url_prefix='/api')
+app.register_blueprint(api_route, url_prefix='/api')
 app.register_blueprint(redirect_route)
