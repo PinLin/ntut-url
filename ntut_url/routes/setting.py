48c3270c586a1f1e33e352a72fb24f99785e7ba9
Pin Lin <moneycat711@gmail.com>
2019-04-11 17:29:55 +0800

    Remove /api/browse

--- ntut_url/routes/setting.py
+++ ntut_url/routes/setting.py
@@ -1,19 +1,8 @@
 import sys
 from flask import Blueprint, request, abort, jsonify
-from ..controller.setting import browse, create
+from ..controller.setting import create
 
 api = Blueprint('setting', __name__)
-
-
-@api.route('/browse', methods=['GET'])
-def setting_browse():
-    code, response = browse.main()
-
-    if code == 200:
-        return jsonify(response)
-
-    else:
-        return abort(code)
 
 
 @api.route('/create', methods=['POST'])
