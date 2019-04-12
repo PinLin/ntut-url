98a1b28026c5535fea3f675e73b15cda6648d3c0
Pin Lin <moneycat711@gmail.com>
2019-04-12 12:27:12 +0800

    Rename find target function

--- ntut_url/controller/find.py
+++ ntut_url/controller/find.py
@@ -3,6 +3,6 @@ from ..models.url import Url
 
 def main(name: str):
     if Url.is_exist(name):
-        return 301, Url(name).target
+        return Url(name).target
     else:
-        return 404, None
+        return None
--- ntut_url/routes/redirect.py
+++ ntut_url/routes/redirect.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, redirect, abort
-from ..controller.get_url import main as get_url
+from ..controller.find import main as find
 
 app = Blueprint('redirect', __name__)
 
 
 @app.route('/<name>', methods=['GET'])
 def redirect_route(name: str):
-    code, target = get_url(name)
+    target = find(name)
 
-    if code // 100 == 3:
-        return redirect(target, code=code)
+    if target:
+        return redirect(target, code=301)
 
     else:
-        return abort(code)
+        return abort(404)
