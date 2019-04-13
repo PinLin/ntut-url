3b531719d6ca9342083028182962b8f1a1892971
Pin Lin <moneycat711@gmail.com>
2019-04-13 10:47:45 +0800

    Redirect to static website

--- ntut_url/routes/redirect.py
+++ ntut_url/routes/redirect.py
@@ -4,6 +4,11 @@ from ..controller.find import main as find
 app = Blueprint('redirect', __name__)
 
 
+@app.route('/', methods=['GET'])
+def root_route():
+    return redirect('/static/index.html', code=302)
+
+
 @app.route('/<name>', methods=['GET'])
 def redirect_route(name: str):
     target = find(name)
