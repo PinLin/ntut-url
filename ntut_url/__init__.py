77e292b29bdacbe29f4252b298c387fd3ad54ddd
Pin Lin <moneycat711@gmail.com>
2019-04-18 14:12:35 +0800

    Split frontend from project

--- ntut_url/__init__.py
+++ ntut_url/__init__.py
@@ -6,5 +6,6 @@ from .routes.redirect import app as redirect_route
 Url.create_table()
 
 app = Flask(__name__)
+app.static_folder = 'static/ntut_url_client'
 app.register_blueprint(api_route, url_prefix='/api')
 app.register_blueprint(redirect_route)
